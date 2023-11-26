a6d8391b7cafc1decd01d900cc0c8bb7c2a7a839
shivanshu814 <shivanshupathak814@gmail.com>
2023-11-27 00:54:49 +0530

    feat: serde_derive Response update using serialize

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use actix_web::{HttpServer,App,web, Responder};
+mod models;
+use crate::models::Status;
+use actix_web::{HttpServer, App, web, Responder, HttpResponse};
 use std::io;
 
-async fn status() -> impl Responder{
-    "{\"status\": \"UP\"}"
+async fn status() -> impl Responder {
+    HttpResponse::Ok()
+    .json(Status{
+        status: "Ok".to_string()
+    })
 }
 
 #[actix_rt::main]
-
-async fn main() -> io::Result<( )>{
-    print!("Starting server at localhost:8080");
+async fn main() -> io::Result<()> {
+    println!("Starting server at localhost:8080");
     HttpServer::new(|| {
         App::new()
         .route("/", web::get().to(status))
